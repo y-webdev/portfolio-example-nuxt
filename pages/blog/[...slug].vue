9f212f176c9b483ae9f04e23b3cc3a67b88c73a1
Yanislav Pavlov <mail@yweb.dev>
2024-04-22 15:38:14 +0200

    - install nuxt content - create some example contents

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute()
+console.log('slug: ', route.params.slug)
+</script>
+
+<template>
+  <article>
+    <ContentDoc />
+  </article>
+</template>
+
+<style scoped>
+
+</style>
