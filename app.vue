f4456dff0c1764c338302d06787ba33127ab20ea
Yanislav Pavlov <mail@yweb.dev>
2024-05-04 15:55:44 +0200

    - page transitions

--- app.vue
+++ app.vue
@@ -7,3 +7,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0.5;
+  filter: blur(1rem);
+}
+</style>
