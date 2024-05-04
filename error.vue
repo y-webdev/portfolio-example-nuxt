10e009b3b19cb576d46edd864e5808d9fe7d9efa
Yanislav Pavlov <mail@yweb.dev>
2024-05-04 15:43:16 +0200

    - error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({ error: Object })
+</script>
+
+<template>
+  <NuxtLayout>
+    <section>
+      <h1 class="text-4xl mb-4">
+        Opps {{ error.statusCode }}
+      </h1>
+
+      <h2 class="text-xl text-gray-500 dark:text-gray-400">
+        {{ error.statusMessage }}
+      </h2>
+    </section>
+  </NuxtLayout>
+</template>
